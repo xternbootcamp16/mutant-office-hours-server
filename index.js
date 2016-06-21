3a4fde78b4c5ae9051f540d2978c179c34b38a60
Seth Baughman <sbaughman1@me.com>
2016-06-21 11:06:41 -0400

    requires firebase library and initializes connection to firebase

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
+// Create references for libraries
 var express = require('express');
 var http = require('http');
+var firebase = require('firebase');
 
+// Express server setup
 var app = express();
 var server = http.createServer(app);
+
+// Authenticate Firebase
+firebase.initializeApp({
+  serviceAccount: "firebase-credentials.json",
+  databaseURL: "https://mutant-hours-am.firebaseio.com"
+});
 
 server.listen(3030, function() {
   console.log('listening on http://localhost:3030');
