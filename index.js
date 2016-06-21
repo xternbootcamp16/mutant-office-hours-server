a363a40c6d12febbaf11bbd7b9281570e97a978d
Seth Baughman <sbaughman1@me.com>
2016-06-21 11:47:05 -0400

    creates twilioClient and creates new text to be sent when texts are added to firebase

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var express = require('express');
 var http = require('http');
 var firebase = require('firebase');
 var dotenv = require('dotenv');
+var twilio = require('twilio');
 
 // Express server setup
 var app = express();
@@ -15,13 +16,25 @@ firebase.initializeApp({
   databaseURL: "https://mutant-hours-am.firebaseio.com"
 });
 
+// Authenticate twilio and create twilio client
+var twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_AUTH_TOKEN);
+
 // Get reference to firebase
 var ref = firebase.database().ref();
 
 // Listen for new texts being created on firebase
 var textsRef = ref.child('texts');
 textsRef.on('child_added', function(snapshot) {
-  console.log(snapshot.val());
+  var text = snapshot.val();
+  twilioClient.messages.create({
+    body: text.name + ', I am available to see you now. Please come to my office so we can discuss: "' + text.topic + '"',
+    to: text.phoneNumber,
+    from: process.env.TWILIO_PHONE
+  }, function(err, message) {
+      if(err) {
+        console.log(err.message);
+      }
+  });
 });
 
 server.listen(3030, function() {
