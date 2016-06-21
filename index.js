e523bf72c58793f19a5e30e8b42c0b26511f8f50
Seth Baughman <sbaughman1@me.com>
2016-06-21 15:05:45 -0400

    add firebase reference library.  Initialize firebase app

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
+// Create references for libraries
 var express = require('express');
 var http = require('http');
+var firebase = require('firebase');
 
+// Express server setup
 var app = express();
 var server = http.createServer(app);
+
+// Authenticate with firebase
+firebase.initializeApp({
+  serviceAccount: "firebase-credentials-pm.json",
+  databaseURL: "https://mutant-hours-pm.firebaseio.com"
+});
 
 server.listen(3030, function() {
   console.log('listening on http://localhost:3030');
