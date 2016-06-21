532616a2bb82c490293ba59e6cc0f7a793d2a01c
Seth Baughman <sbaughman1@me.com>
2016-06-21 11:23:02 -0400

    require dotenv and load it.  Add reference to firebase.  Create method to listen for new texts being added and log the response to the console

--- index.js
+++ index.js
@@ -2,15 +2,26 @@
 var express = require('express');
 var http = require('http');
 var firebase = require('firebase');
+var dotenv = require('dotenv');
 
 // Express server setup
 var app = express();
 var server = http.createServer(app);
+dotenv.load();
 
 // Authenticate Firebase
 firebase.initializeApp({
   serviceAccount: "firebase-credentials.json",
   databaseURL: "https://mutant-hours-am.firebaseio.com"
+});
+
+// Get reference to firebase
+var ref = firebase.database().ref();
+
+// Listen for new texts being created on firebase
+var textsRef = ref.child('texts');
+textsRef.on('child_added', function(snapshot) {
+  console.log(snapshot.val());
 });
 
 server.listen(3030, function() {
