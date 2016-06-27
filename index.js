836b20634989141f36834017f4d9a22b11e599b0
Seth Baughman <sbaughman1@me.com>
2016-06-27 12:53:15 -0400

    Add mailgun and set up mailgunClient to send email on child_added to Firebase

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var http = require('http');
 var firebase = require('firebase');
 var dotenv = require('dotenv');
 var twilio = require('twilio');
+var mailgun = require('mailgun-js');
 
 // Express server setup
 var app = express();
@@ -12,15 +13,18 @@ dotenv.load();
 
 // Authenticate Firebase
 firebase.initializeApp({
-  serviceAccount: "firebase-credentials.json",
-  databaseURL: "https://mutant-hours-am.firebaseio.com"
+  serviceAccount: "firebase-credentials-pm.json",
+  databaseURL: "https://mutant-hours-pm.firebaseio.com"
 });
-
-// Authenticate twilio and create twilio client
-var twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_AUTH_TOKEN);
 
 // Get reference to firebase
 var ref = firebase.database().ref();
+
+// Authenticate mailgun and create mailgun client
+var mailgunClient = mailgun({apiKey: process.env.MAILGUN_KEY, domain: process.env.MAILGUN_DOMAIN});
+
+// Authenticate twilio and create twilio client
+var twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_AUTH_TOKEN);
 
 // Listen for new texts being created on firebase
 var textsRef = ref.child('texts');
@@ -34,6 +38,24 @@ textsRef.on('child_added', function(snapshot) {
       if(err) {
         console.log(err.message);
       }
+  });
+});
+
+// Listen for new emails being added and send an email through mailgun
+var emailsRef = ref.child('emails');
+emailsRef.on('child_added', function(snapshot) {
+  var email = snapshot.val();
+  var emailData = {
+    from: '<postmaster@' + process.env.MAILGUN_DOMAIN + '>',
+    to: email.emailAddress,
+    subject: 'Thanks for Registering!',
+    text: 'Welcome to Mutant Office Hours.  Thanks for signing up! tis awesumm',
+  };
+  mailgunClient.messages().send(emailData, function(error, body) {
+    console.log(body);
+    if (error) {
+      console.log(error);
+    }
   });
 });
 
