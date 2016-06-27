be10181e995e28ae79c967e422ce84aa9cdbc956
Seth Baughman <sbaughman1@me.com>
2016-06-27 16:07:28 -0400

    Install mailgun-js and require library.  Set up mailgunClient and send email when child_added to emails on firebase

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var http = require('http');
 var firebase = require('firebase');
 var twilio = require('twilio');
 var dotenv = require('dotenv');
+var mailgun = require('mailgun-js');
 
 // Express server setup
 var app = express();
@@ -16,6 +17,9 @@ firebase.initializeApp({
   databaseURL: "https://mutant-hours-pm.firebaseio.com"
 });
 var rootRef = firebase.database().ref();
+
+// Authenticate with mailgun
+var mailgunClient = mailgun({apiKey: process.env.MAILGUN_KEY, domain: process.env.MAILGUN_DOMAIN });
 
 // Authenticate with twilio
 var twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_AUTH_TOKEN);
@@ -32,6 +36,24 @@ textsRef.on('child_added', function(snapshot) {
       if(err) {
         console.log(err.message);
       }
+  });
+});
+
+// Listen for new emails being added to firebase and send email through mailgun
+var emailsRef = rootRef.child('emails');
+emailsRef.on('child_added', function(snapshot) {
+  var email = snapshot.val();
+  var emailData = {
+    from: '<postmaster@' + process.env.MAILGUN_DOMAIN +'>',
+    to: email.emailAddress,
+    subject: 'Thanks for signing up!',
+    text: 'Welcome to Mutant Office Hours.  It is sweet.'
+  };
+  mailgunClient.messages().send(emailData, function(error, body) {
+    console.log(body);
+    if (error) {
+      console.log(error);
+    }
   });
 });
 
